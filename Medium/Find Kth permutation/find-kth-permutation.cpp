f3b25110e48dd74108df8a7e3fc53196efd7359c
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-06-08 07:54:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Kth permutation/find-kth-permutation.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    string kthPermutation(int n, int k){
+        string s;
+        for(int i=1; i<n+1; i++) s.push_back('0'+i);
+        for(int i=0; i<k-1; i++) next_permutation(s.begin(), s.end());
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        Solution ob;
+        string ans = ob.kthPermutation(n, k);
+        cout << ans;
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
