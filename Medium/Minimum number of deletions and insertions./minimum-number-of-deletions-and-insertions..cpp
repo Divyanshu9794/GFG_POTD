8a8c2e41983496addd5805354d8e1f2402f35fb4
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-06-30 11:29:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum number of deletions and insertions./minimum-number-of-deletions-and-insertions..cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	
+int lcs(string s, string t)
+{
+    int n=s.size(),m=t.size();
+    int dp[n+1][m+1];
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            if(i==0 or j==0){
+                dp[i][j]=0;
+            }
+            else if(s[i-1]==t[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+
+int minOperations(string &str, string &ptr)
+{
+    return str.length()+ptr.length() - 2* lcs(str,ptr);
+}
+	
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
