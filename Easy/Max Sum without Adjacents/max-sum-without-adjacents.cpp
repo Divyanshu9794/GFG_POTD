140a53f86786765b3912ea2f018678493669a9cf
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-07-19 11:52:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// calculate the maximum sum with out adjacent
+		 int f(int ind,int *nums,vector<int>& dp){
+       
+        if(ind==0) return nums[ind];
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int p= nums[ind] + f(ind-2,nums,dp);
+        int np = 0 + f(ind-1,nums,dp);
+
+        return dp[ind]=max(p,np);
+ }
+    int findMaxSum(int *nums, int n){
+    // Write your code here.
+      
+       vector<int> dp(n,-1);
+       return f(n-1,nums,dp);
+}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaxSum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
