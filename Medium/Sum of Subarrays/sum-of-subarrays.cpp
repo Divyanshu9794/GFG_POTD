811473a0f7dd1458e3c15eb1c978c351e3027783
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-07-22 11:15:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Subarrays/sum-of-subarrays.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    long long subarraySum(long long a[], long long n)
+    {
+        long long int sum=0,mod=1e9+7;
+        for(int i=0;i<n;i++){
+            sum+=(a[i]*(i+1)*(n-i))%mod;
+        }
+        return sum%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     long long t; 
+     cin>>t;
+     while(t--)
+     {
+         long long n; cin>>n;
+         long long a[n+5];
+         for(long long i=0;i<n;i++)
+         cin>>a[i];
+         Solution ob;
+         cout<<ob.subarraySum(a, n)<<endl;
+     }
+    return 0;
+}
+
+// } Driver Code Ends
