7488e2bad14f836521621489f1d4fc5f1b9490b5
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-07-27 14:54:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
+                     pair<int, int> destination) {
+        
+        if (source == destination) {
+            return 0;
+        }
+        queue<pair<int, pair<int, int>>>q;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>dist(n, vector<int>(m, 1e9));
+        dist[source.first][source.second]=0;
+        q.push({0, {source.first, source.second}});
+        int drow[]={-1, 0, 1, 0};
+        int dcol[]={0, 1, 0, -1};
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int d=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
+            
+            for(int i=0; i<4; i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && d+1<dist[nrow][ncol]){
+                    dist[nrow][ncol]=d+1;
+                    if(nrow==destination.first && ncol==destination.second){
+                        return d+1;
+                    }
+                    q.push({d+1, {nrow, ncol}});
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+
+        pair<int, int> source, destination;
+        cin >> source.first >> source.second;
+        cin >> destination.first >> destination.second;
+        Solution obj;
+        cout << obj.shortestPath(grid, source, destination) << endl;
+    }
+}
+
+// } Driver Code Ends
