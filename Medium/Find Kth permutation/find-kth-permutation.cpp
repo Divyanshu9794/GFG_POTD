186c1154f8c2b1e1804c43dbc6511cf978cb5711
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-08-03 09:26:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find Kth permutation/find-kth-permutation.cpp
+++ Medium/Find Kth permutation/find-kth-permutation.cpp
@@ -11,11 +11,28 @@ using namespace std;
 class Solution
 {
 public:
-    string kthPermutation(int n, int k){
-        string s;
-        for(int i=1; i<n+1; i++) s.push_back('0'+i);
-        for(int i=0; i<k-1; i++) next_permutation(s.begin(), s.end());
-        return s;
+    string kthPermutation(int n, int k)
+    {
+        // code here
+        int factorial = 1;
+        vector<int> number;
+        for(int i=1;i<n;i++){
+            factorial = factorial*i;
+            number.push_back(i);
+        }
+        number.push_back(n);
+        string ans="";
+        k=k-1;
+        while(true){
+            ans = ans + to_string(number[k/factorial]);
+            number.erase(number.begin() + k/factorial);
+            if(number.size()==0){
+                break;
+            }
+            k=k%factorial;
+            factorial = factorial/number.size();
+        }
+        return ans;
     }
 };
 
