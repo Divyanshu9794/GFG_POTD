634ad3f1c94e6d282faa2b7944fc50a046853546
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-08-26 10:05:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Validate an IP Address/validate-an-ip-address.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function returns 1 if
+IP string is valid else return 0
+You are required to complete this method */
+class Solution {
+    public:
+        int isValid(string s) {
+            // code here
+            
+            int dotc=0;
+            for(int i=0;i<s.size();i++){
+                if(s[i]=='.'){
+                    dotc++;
+                }
+            }
+            if(dotc!=3){
+                return 0;
+            }
+            dotc=0;
+            unordered_set<string> st;
+            for(int i=0;i<256;i++){
+                st.insert(to_string(i));
+            }
+            string t="";
+            for(int i=0;i<s.size();i++){
+                if(s[i]!='.'){
+                    t+=s[i];
+                }
+                else{
+                    if(st.count(t)!=0){
+                        dotc++;
+                    }
+                    t="";
+                }
+            }
+            if(st.count(t)!=0){
+                dotc++;
+            }
+            if(dotc==4){
+                return 1;
+            }
+            return 0;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isValid(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
