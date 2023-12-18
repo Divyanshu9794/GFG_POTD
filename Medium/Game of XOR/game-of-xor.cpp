43650b8a2379212169b92b7e4bcd71446436dfa3
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-12-18 17:14:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        // code here
+        int res=0;
+        if(N%2==0){
+            return res;
+        }
+        for(int i=0;i<N;i+=2){
+            res ^= A[i];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
