9a6eeeaff71a2f30b37eb0b21268a5a8430555a2
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-01-04 00:13:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        unordered_map<int,int> mp;
+        int a;
+        for(int i=0;i<N;i++){
+            mp[arr[i]]++;
+        }
+        for(auto &x:mp){
+            if(x.second==1){
+                a= x.first;
+            }
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
