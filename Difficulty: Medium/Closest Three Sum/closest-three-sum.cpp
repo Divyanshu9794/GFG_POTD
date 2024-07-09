44d580df63cbfe0d8c005953e39f057c88d5a54f
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-07-09 17:37:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Closest Three Sum/closest-three-sum.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+// arr : given vector of elements
+// target : given sum value
+
+class Solution {
+  public:
+    int threeSumClosest(vector<int> arr, int target) {
+        sort(arr.begin(), arr.end());
+        
+        int prevDiff = INT_MAX;
+        int closestSum = INT_MIN;
+        for(int i = 0; i < arr.size() - 2; i++){
+            int start = i + 1; 
+            int end = arr.size() - 1;
+            while(start < end){
+                int sum = arr[i] + arr[start] + arr[end];
+                if(sum == target) return sum;
+                int currentDiff = abs(sum - target);
+                if(currentDiff < prevDiff || (currentDiff == prevDiff && sum > closestSum)){
+                    prevDiff = currentDiff;
+                    closestSum = sum;
+                }
+                if(sum < target){
+                    start++;
+                }
+                else{
+                    end--;
+                }
+            }
+        }
+        return closestSum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        // cin.ignore();
+        // if (f)
+        //     return 0;
+        Solution ob;
+        cout << ob.threeSumClosest(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
