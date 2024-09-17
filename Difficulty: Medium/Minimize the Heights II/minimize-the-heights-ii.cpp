850c6bdc52a7cf2b5eb9092c4d1de0b597cf2876
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-09-17 11:04:03 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        
+        sort(arr.begin(), arr.end());
+        int difference = arr[n-1]-arr[0];
+        for(int i = 1; i<n; i++)
+        {
+            if(arr[i]-k<0){
+                continue;
+            }
+            int mini = min(arr[0]+k, arr[i]-k); 
+            int maxi = max(arr[i-1]+k, arr[n-1]-k); 
+            difference = min(difference, maxi-mini);
+        }
+        return difference;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin.ignore();
+        vector<int> a, b, c, d;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution ob;
+        auto ans = ob.getMinDiff(a, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
