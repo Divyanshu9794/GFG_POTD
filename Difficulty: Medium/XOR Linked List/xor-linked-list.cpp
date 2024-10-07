795328a09d796f65b235483a07cf8c703641d18b
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-10-07 10:27:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/XOR Linked List/xor-linked-list.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <stdint.h>
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *npx;
+
+    Node(int x) {
+        data = x;
+        npx = NULL;
+    }
+};
+
+struct Node *XOR(struct Node *a, struct Node *b) {
+    return (struct Node *)((uintptr_t)(a) ^ (uintptr_t)(b));
+}
+
+struct Node *insert(struct Node *head, int data);
+
+vector<int> getList(struct Node *head);
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        struct Node *head = NULL;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            head = insert(head, number);
+        }
+
+        vector<int> vec = getList(head);
+        for (int x : vec)
+            cout << x << " ";
+        cout << endl;
+        for (int i = vec.size() - 1; i >= 0; i--) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+    }
+    return (0);
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of linked list is as
+struct Node
+{
+    int data;
+    struct Node* npx;
+
+    Node(int x){
+        data = x;
+        npx = NULL;
+    }
+};
+
+Utility function to get XOR of two Struct Node pointer
+use this function to get XOR of two pointers
+struct Node* XOR (struct Node *a, struct Node *b)
+{
+    return (struct Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));
+}
+*/
+
+// function should insert the data to the front of the list
+struct Node *insert(struct Node *head, int data) {
+    // Code here
+    if(head==NULL){
+        return new Node(data);
+        
+    }
+    else{
+        Node* root = new Node(data);
+        root->npx = head;
+        return root;
+    }
+}
+
+vector<int> getList(struct Node *head) {
+    // Code here
+    vector<int> ans;
+    while(head){
+        ans.push_back(head->data);
+        head = head ->npx;
+    }
+    return ans;
+}
