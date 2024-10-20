a2f62582ddf74106d1e72121392942e750cf56ed
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-10-20 10:49:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort a k sorted doubly linked list/sort-a-k-sorted-doubly-linked-list.cpp
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// a node of the doubly linked list
+class DLLNode {
+  public:
+    int data;
+    DLLNode *next;
+    DLLNode *prev;
+
+    DLLNode(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*
+// a node of the doubly linked list
+class DLLNode
+{
+public:
+    int data;
+    DLLNode *next;
+    DLLNode *prev;
+    DLLNode(int val)
+    {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // function to sort a k sorted doubly linked list
+    DLLNode *sortAKSortedDLL(DLLNode *head, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        DLLNode* ptr=head;
+        DLLNode* curr=head;
+        DLLNode* ans=NULL;
+        while(k>=0 && ptr!=NULL){
+            pq.push(ptr->data);
+            ptr=ptr->next;
+            k--;
+        }
+        while(ptr!=NULL){
+            curr->data=pq.top();
+            pq.pop();
+           if(ans==NULL)
+           ans=curr;
+            pq.push(ptr->data);
+            ptr=ptr->next;
+            curr=curr->next;
+        }
+        while(pq.empty()==false){
+            curr->data=pq.top();
+            pq.pop();
+            if(ans==NULL)
+           ans=curr;
+            curr=curr->next;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Function to insert a node at the end
+// of the Doubly Linked List
+void push(DLLNode **tailRef, int new_data) {
+    // allocate node
+    DLLNode *newNode = new DLLNode(new_data);
+
+    // since we are adding at the end,
+    // next is NULL
+    newNode->next = NULL;
+
+    newNode->prev = (*tailRef);
+
+    // change next of tail node to new node
+    if ((*tailRef) != NULL)
+        (*tailRef)->next = newNode;
+
+    (*tailRef) = newNode;
+}
+
+// Function to print nodes in a given doubly linked list
+void printList(DLLNode *head) {
+    // if list is empty
+    if (head == NULL)
+        return;
+
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+// Driver
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        DLLNode *head = new DLLNode(arr[0]);
+        DLLNode *tail = head;
+
+        // Check if the array is empty
+        for (size_t i = 1; i < arr.size(); ++i) {
+            push(&tail, arr[i]);
+        }
+        Solution obj;
+        printList(obj.sortAKSortedDLL(head, k));
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
