bed15544c6ace6913170b5a8ac373f3a52ceb58e
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-10-30 18:21:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with difference k/pairs-with-difference-k.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        unordered_map<int,int>mp;
+
+        int c=0;
+
+        for(auto num:arr){
+
+            if(mp.count(num-k)){
+
+                c+=mp[num-k];
+
+            }   
+
+            if(mp.count(num+k)){
+
+                c+=mp[num+k];
+
+            }
+
+        mp[num]++;
+
+        }
+
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
