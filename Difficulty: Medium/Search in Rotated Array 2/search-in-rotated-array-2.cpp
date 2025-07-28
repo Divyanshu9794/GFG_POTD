8c307e49658f92e349ae0d7857fe84eccd80bfc9
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-07-28 16:41:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Array 2/search-in-rotated-array-2.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) return true;
+
+            // When we can't determine the sorted part due to duplicates
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+            // Left half is sorted
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
