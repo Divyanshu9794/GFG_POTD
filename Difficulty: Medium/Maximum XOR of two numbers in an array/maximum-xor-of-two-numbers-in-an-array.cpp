221a11f99fc2f83980bd2ed177a66534ea092786
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-08-19 12:22:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum XOR of two numbers in an array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,66 @@
+// User function Template for C++
+
+struct Node {
+    Node* links[2]; 
+    bool containsKey(int bit) {
+        return (links[bit] != NULL); 
+    }
+    Node* get(int bit) {
+        return links[bit]; 
+    }
+    void put(int bit, Node* node) {
+        links[bit] = node; 
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(int num) {
+        Node* node = root; 
+        for (int i = 31; i >= 0; i--) { 
+            int bit = (num >> i) & 1; 
+            if (!node->containsKey(bit)) { 
+                node->put(bit, new Node()); 
+            }
+            
+            node = node->get(bit); 
+        }
+    }
+    
+    int getMax(int num) {
+        Node* node = root;
+        int maxNum = 0; 
+        for (int i = 31; i >= 0; i--) { 
+            int bit = (num >> i) & 1; 
+            if (node->containsKey(1 - bit)) { 
+                maxNum |= (1 << i); 
+                node = node->get(1 - bit);
+            } else {
+                node = node->get(bit);
+            }
+        }
+        return maxNum; 
+    }
+};
+
+
+class Solution {
+public:
+    int maxXor(vector<int>& nums) {
+        Trie trie; 
+        for (auto& it : nums) { 
+            trie.insert(it);
+        }
+        int maxi = 0; 
+        for (auto& it : nums) { 
+            maxi = max(maxi, trie.getMax(it));
+        }
+        return maxi;
+        }
+};
