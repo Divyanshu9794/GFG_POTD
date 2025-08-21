6a195a38ecdcc8b0d2c5884686218c7bc52ce06f
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-08-21 12:29:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Merge two binary Max heaps/merge-two-binary-max-heaps.cpp
@@ -0,0 +1,39 @@
+// User function Template for C++
+
+class Solution{
+    public:
+    void heapify(vector<int > &arr,int n,int i){
+        int largest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        
+        if(left<n && arr[largest]<arr[left]){
+            largest=left;
+        }
+        if(right<n && arr[largest]<arr[right]){
+            largest=right;
+        }
+        if(largest!=i){
+            swap(arr[largest],arr[i]);
+            heapify(arr,n,largest);
+        }
+    }
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        // your code here
+        
+        vector<int> ans;
+      
+        for(auto i:a){
+            ans.push_back(i);
+            
+        }
+        for(auto i:b){
+            ans.push_back(i);
+        }
+        int s = ans.size(); 
+        for(int i=s/2-1;i>=0;i--){
+            heapify(ans,s,i);
+        }
+        return ans;
+    }
+};
