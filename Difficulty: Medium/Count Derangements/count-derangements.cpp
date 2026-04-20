d7e679bcac67e6d480b73457125d3f3c1f7a522e
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2026-04-20 21:31:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Derangements/count-derangements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int derangeCount(int n) {
+        // code here
+        if(n==1) return 0;
+        if(n==2) return 1;
+        int x=0,y=1;
+        for(int i=3;i<=n;i++){
+            int p=(i-1)*(x+y);
+            x=y;
+            y=p;
+        }
+        return y;
+    }
+};
