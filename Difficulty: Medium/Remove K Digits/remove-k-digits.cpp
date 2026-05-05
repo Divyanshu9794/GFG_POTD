b3c82ff39ada089adacc6fc2d4f7a7b8408f5ad9
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2026-05-05 15:01:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove K Digits/remove-k-digits.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    string removeKdig(string num, int k) {
+        string res="";
+        
+        for(auto c : num){
+				while(res.length() && res.back()>c && k){ 
+                res.pop_back();
+                k--;
+            }
+            
+            if(res.length() || c!='0')
+                res.push_back(c);
+        }
+        
+        while(res.length() && k){
+            k--;
+            res.pop_back();
+        }
+        
+        return res.empty()? "0" : res;      
+    }
+};
